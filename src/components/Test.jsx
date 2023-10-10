a3e9ce8e2e6ab4bc64e7de1106c6e64585e54899
祖士洋 <zushiyang@idataway.com>
2023-10-10 18:23:09 +0800

    feat: asyncUpdate

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,20 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 function Test (props) {
   // 声明一个新的叫做 “count” 的 state
-  console.log(props)
+  // console.log(props)
   const [count, setCount] = useState(0)
   const arr = [1, 2, 3]
-  console.log(Object.isFrozen(arr))
+  // console.log(Object.isFrozen(arr))
+  const arrLi = arr.map((item) => {
+    return <li key={item}>{item}</li>
+  })
+  const increment = () => {
+    setCount(count => count + 1)
+    console.log(count)
+  }
+  useEffect(() => {
+    console.log(count)
+  })
   return (
         <>
-            <p style={{ color: 'red' }}>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me {arr}
+            <p style={{ color: 'red' }}>counts: {count}</p>
+            <button onClick={increment}>
+                Click me +1
+            </button>
+            <button onClick={() => { increment(); increment(); increment() }}>
+                Click me +3
             </button>
             <ul>
-                {arr.map((item) => {
-                  return <li key={item}>{item}</li>
-                })}
+                {arrLi}
             </ul>
         </>
   )
